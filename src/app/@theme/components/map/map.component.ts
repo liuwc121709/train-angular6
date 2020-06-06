eac583ac8091bda1f45ee46acf5d154c3cfd5d0d
wuxiaoxu <wuxiaoxu@golaxy.com>
2020-06-06 13:28:59 +0800

    update

--- src/app/@theme/components/map/map.component.ts
+++ src/app/@theme/components/map/map.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, OnChanges, Input, Output, SimpleChanges, EventEmitte
   styleUrls: ['./map.component.css']
 })
 
-export class MapComponent  implements OnInit, OnChanges {
+export class MapComponent implements OnInit, OnChanges {
   @Input() config: any;
   @Input() data: any;
   @Output() refresh = new EventEmitter();
@@ -47,11 +47,11 @@ export class MapComponent  implements OnInit, OnChanges {
     });
   }
 
-  ngOnChanges (changes: SimpleChanges) {
+  ngOnChanges(changes: SimpleChanges) {
     if (!changes.data.firstChange) {
       // init data
       this.currentMapData = {};
-      this.currentMapData.area = changes.data.currentValue.area;
+      this.currentMapData.area = changes.data.currentValue.area === undefined ? {} : changes.data.currentValue.area;
       this.currentMapData.mapData = changes.data.currentValue.mapData;
       this.currentMapData.config = this.currentMapDataConfig;
 
@@ -60,8 +60,8 @@ export class MapComponent  implements OnInit, OnChanges {
 
       // 收集搜集数据
       Promise.all(promises).then(values => {
-          // 初始化数据
-          this.addMarker();
+        // 初始化数据
+        this.addMarker();
       }).catch(reason => {
         console.log(reason);
       });
@@ -71,136 +71,136 @@ export class MapComponent  implements OnInit, OnChanges {
   load(ak: string): Promise<any> {
     const Map_URL = '//api.map.baidu.com/api?v=2.0&ak=' + ak + '&callback=JSONP_CALLBACK';
     const p = new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.setAttribute('src', Map_URL);
-        script.async = true;
-        script.onload = function () {
-            script.parentNode.removeChild(script);
-        };
-        window['JSONP_CALLBACK'] = function (json) {
-            resolve(json);
-        };
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.setAttribute('src', Map_URL);
+      script.async = true;
+      script.onload = function () {
+        script.parentNode.removeChild(script);
+      };
+      window['JSONP_CALLBACK'] = function (json) {
+        resolve(json);
+      };
 
-        document.head.appendChild(script);
+      document.head.appendChild(script);
     });
 
     return p;
-}
+  }
 
   initBMapClass() {
-      // 从api中获取BMap类
-      const BMap = (<any>window)['BMap'];
-      this.BMapClass = BMap;
+    // 从api中获取BMap类
+    const BMap = (<any>window)['BMap'];
+    this.BMapClass = BMap;
   }
 
   initBMapInstance() {
-      // 类
-      const BMap = this.BMapClass;
+    // 类
+    const BMap = this.BMapClass;
 
-      // 创建Map实例
-      const map = new BMap.Map(this.rootEl, {
-        enableMapClick: false,
-        minZoom: 10
-      });
-      this.MapInstance = map;
+    // 创建Map实例
+    const map = new BMap.Map(this.rootEl, {
+      enableMapClick: false,
+      minZoom: 10
+    });
+    this.MapInstance = map;
   }
 
   initBMapEnvent() {
-      // BMapClass
-      const BMap = this.BMapClass;
-      // MapInstance
-      const map = this.MapInstance;
+    // BMapClass
+    const BMap = this.BMapClass;
+    // MapInstance
+    const map = this.MapInstance;
 
-      // $this
-      const $this = this;
+    // $this
+    const $this = this;
 
-      // 初始化地图
-      // 第一个参数, 设置中心点坐标，可以使用中文或英文
-      // 第二个参数, 设置地图级别, 级别为0 ~ 19, 数字越大，越详细
-      map.centerAndZoom(new BMap.Point(116.403694, 39.916042), 11);
-      // map.centerAndZoom("北京", 11);
-      // map.centerAndZoom("Hongkong", 11);
+    // 初始化地图
+    // 第一个参数, 设置中心点坐标，可以使用中文或英文
+    // 第二个参数, 设置地图级别, 级别为0 ~ 19, 数字越大，越详细
+    map.centerAndZoom(new BMap.Point(116.403694, 39.916042), 11);
+    // map.centerAndZoom("北京", 11);
+    // map.centerAndZoom("Hongkong", 11);
 
-      // 利用鼠标滚轮控制大小
-      map.enableScrollWheelZoom(true);
+    // 利用鼠标滚轮控制大小
+    map.enableScrollWheelZoom(true);
 
-      // 监听地图级别缩放事件，显示不同级别的数据
-      map.addEventListener('zoomend', function() {
-        // 获取地图缩放级别
-        const zoomLevel = map.getZoom();
-        // 根据缩放来显示对应内容
-        $this.config.dataConfig.forEach(dataItem => {
-            if (zoomLevel >= dataItem.zoomRange[0] && zoomLevel <= dataItem.zoomRange[1]) {
-                // currentDataConfig
-                $this.currentMapDataConfig = dataItem;
-                $this.refresh.emit(dataItem.type);
-            }
-        });
+    // 监听地图级别缩放事件，显示不同级别的数据
+    map.addEventListener('zoomend', function () {
+      // 获取地图缩放级别
+      const zoomLevel = map.getZoom();
+      // 根据缩放来显示对应内容
+      $this.config.dataConfig.forEach(dataItem => {
+        if (zoomLevel >= dataItem.zoomRange[0] && zoomLevel <= dataItem.zoomRange[1]) {
+          // currentDataConfig
+          $this.currentMapDataConfig = dataItem;
+          $this.refresh.emit(dataItem.type);
+        }
       });
+    });
 
-      // 监听地图移动,根据视野动态加载
-      map.addEventListener('moveend', function() {
-        // 获取地图缩放级别
-        const zoomLevel = map.getZoom();
-        // // 根据缩放来显示对应内容
-        // $this.config.dataConfig.forEach(dataItem => {
-        //   if (zoomLevel >= dataItem.zoomRange[0] && zoomLevel <= dataItem.zoomRange[1]) {
-        //       // currentDataConfig
-        //       $this.currentMapDataConfig = dataItem;
-        //       $this.refresh.emit(dataItem.type);
-        //   }
-        // });
-      });
+    // 监听地图移动,根据视野动态加载
+    map.addEventListener('moveend', function () {
+      // 获取地图缩放级别
+      const zoomLevel = map.getZoom();
+      // // 根据缩放来显示对应内容
+      // $this.config.dataConfig.forEach(dataItem => {
+      //   if (zoomLevel >= dataItem.zoomRange[0] && zoomLevel <= dataItem.zoomRange[1]) {
+      //       // currentDataConfig
+      //       $this.currentMapDataConfig = dataItem;
+      //       $this.refresh.emit(dataItem.type);
+      //   }
+      // });
+    });
   }
 
   refreshMap(type) {
-      if (!type) {
-        type = this.config.dataConfig[0].type;
-      }
+    if (!type) {
+      type = this.config.dataConfig[0].type;
+    }
 
-      this.refresh.emit(type);
+    this.refresh.emit(type);
   }
 
   /**
    * 设置不同类型的marker: circle, rectangle
    */
   addMarker() {
-      // MapInstance
-      const map = this.MapInstance;
+    // MapInstance
+    const map = this.MapInstance;
 
-      // 当前要显示数据层的配置信息
-      const curConf = this.currentMapData.config;
-      const mapData = this.currentMapData.mapData;
-      const areaData = this.currentMapData.area;
+    // 当前要显示数据层的配置信息
+    const curConf = this.currentMapData.config;
+    const mapData = this.currentMapData.mapData;
+    const areaData = this.currentMapData.area;
 
-      // 初始化数据
-      this.cacheRectangle = [];
-      this.cachePolygonAll = [];
+    // 初始化数据
+    this.cacheRectangle = [];
+    this.cachePolygonAll = [];
 
-      // $this
-      const $this = this;
+    // $this
+    const $this = this;
 
-      // 清空当前地图显示
-      map.clearOverlays();
+    // 清空当前地图显示
+    map.clearOverlays();
 
-      // 循环处理展示数据项
-      for (let index = 0; index < mapData.length; index++) {
-        // itemData
-        const itemData = mapData[index];
+    // 循环处理展示数据项
+    for (let index = 0; index < mapData.length; index++) {
+      // itemData
+      const itemData = mapData[index];
 
-        // 如果有bound，将会对元素绘制区域
-        if (curConf.bound) {
-          // 绘画行政边界
-          this.setBoundary(itemData, areaData);
-        }
-
-        // 设置label
-        this.labelFactory(curConf.mode, itemData);
+      // 如果有bound，将会对元素绘制区域
+      if (curConf.bound) {
+        // 绘画行政边界
+        this.setBoundary(itemData, areaData);
       }
 
-      // 根据视野动态加载
-      // this.addViewLabel(this.cacheRectangle);
+      // 设置label
+      this.labelFactory(curConf.mode, itemData);
+    }
+
+    // 根据视野动态加载
+    // this.addViewLabel(this.cacheRectangle);
   }
 
   labelFactory(mode, itemData) {
@@ -234,10 +234,10 @@ export class MapComponent  implements OnInit, OnChanges {
 
     // 自定义label
     const tpl = '<div class="bubble bubble-1" data-longitude="' + itemData.longitude + '"' +
-    ' data-latitude="' + itemData.latitude + '">' +
-    '<p class="name" title="' + itemData.title + '">' + itemData.title + '</p>' +
-    '<p class="count"><span>' + itemData.count + '</span>公司</p>' +
-    '</div>';
+      ' data-latitude="' + itemData.latitude + '">' +
+      '<p class="name" title="' + itemData.title + '">' + itemData.title + '</p>' +
+      '<p class="count"><span>' + itemData.count + '</span>公司</p>' +
+      '</div>';
 
     // label 在此处添加点位位置信息
     const label = new BMap.Label(tpl, {
@@ -259,7 +259,7 @@ export class MapComponent  implements OnInit, OnChanges {
     label.setTitle(itemData.title);
 
     // 当鼠标悬停在label上时显示行政区划边界
-    label.addEventListener('mouseover', function() {
+    label.addEventListener('mouseover', function () {
       // 修改覆盖物背景颜色
       label.setStyle({
         background: '#E2A76E',
@@ -273,7 +273,7 @@ export class MapComponent  implements OnInit, OnChanges {
     });
 
     // 当鼠标离开时在删除边界折线数据
-    label.addEventListener('mouseout', function() {
+    label.addEventListener('mouseout', function () {
       // 修改覆盖物背景颜色
       label.setStyle({
         background: '#46ACFF',
@@ -286,7 +286,7 @@ export class MapComponent  implements OnInit, OnChanges {
       }
     });
 
-    label.addEventListener('click', function() {
+    label.addEventListener('click', function () {
       const curPoint = label.getPosition();
 
       // 有下一层次数据显示，将会重新设置centerAndZoom的第二个参数zoom
@@ -332,7 +332,7 @@ export class MapComponent  implements OnInit, OnChanges {
       lineHeight: '22px',
       border: '1px solid #46ACFF', // 边
       borderRadius: '2px',
-      background:  '#fff', // 背景颜色
+      background: '#fff', // 背景颜色
       opacity: 0.9,
       cursor: 'pointer',
       zIndex: 2
@@ -340,7 +340,7 @@ export class MapComponent  implements OnInit, OnChanges {
     label.setTitle(itemData.title);
 
     // 修改覆盖物背景颜色
-    label.addEventListener('mouseover', function() {
+    label.addEventListener('mouseover', function () {
       label.setStyle({
         // background: '#E2A76E',
         zIndex: 4
@@ -348,14 +348,14 @@ export class MapComponent  implements OnInit, OnChanges {
     });
 
     // 修改覆盖物背景颜色
-    label.addEventListener('mouseout', function() {
+    label.addEventListener('mouseout', function () {
       label.setStyle({
         background: '#fff',
         zIndex: 2
       });
     });
 
-    label.addEventListener('click', function() {});
+    label.addEventListener('click', function () { });
 
     // 添加点位
     map.addOverlay(label);
@@ -372,15 +372,12 @@ export class MapComponent  implements OnInit, OnChanges {
     if (!name) {
       name = itemData.title;
     }
-    try {
-      // operation
-      if (type === 'show') {
-        this.cachePolygonAll[name].show();
-      } else if (type === 'hide') {
-        this.cachePolygonAll[name].hide();
-      }
-    } catch(e) {
-      debugger;
+
+    // operation
+    if (type === 'show') {
+      this.cachePolygonAll[name].show();
+    } else if (type === 'hide') {
+      this.cachePolygonAll[name].hide();
     }
   }
 
@@ -411,8 +408,8 @@ export class MapComponent  implements OnInit, OnChanges {
 
     // 检查类型是否正确
     if (!(point instanceof BMap.Point) ||
-        !(bounds instanceof BMap.Bounds)) {
-        return false;
+      !(bounds instanceof BMap.Bounds)) {
+      return false;
     }
 
     // 西南脚点
@@ -464,65 +461,83 @@ export class MapComponent  implements OnInit, OnChanges {
 
     // 如果后台传了area数据，就不自动搜集了
     let bound = curConf.bound;
-    if (bound && areaData) {
-        bound = false;
+    if (bound && areaData !== undefined && areaData.length > 0) {
+      bound = false;
     }
 
     // promises
     const promises = [];
 
     // 坐标开启的化，使用LocalSearch自动搜集地理坐标
-    if (curConf.coordinate || bound) {
-        // BMapClass
-        const BMap = this.BMapClass;
-        // MapInstance
-        const map = this.MapInstance;
+    if (!curConf.coordinate && !bound) {
+      return promises;
+    }
 
-        // 循环处理
-        for (let index = 0; index < mapData.length; index++) {
-          // itemData
-          const itemData = mapData[index];
+    // BMapClass
+    const BMap = this.BMapClass;
+    // MapInstance
+    const map = this.MapInstance;
 
-          // 名字
-          let name = itemData.name;
-          if (!name) {
-            name = itemData.title;
-          }
+    // 循环处理
+    for (let index = 0; index < mapData.length; index++) {
+      // itemData
+      const itemData = mapData[index];
 
-          const p = new Promise((resolve, reject) => {
-            // 搜集地理位置信息
-            const localSearch = new BMap.LocalSearch(map);
+      // 名字
+      let name = itemData.name;
+      if (!name) {
+        name = itemData.title;
+      }
 
-            // 根据具体名字查找
-            localSearch.search(name);
+      // 坐标开启，使用LocalSearch自动搜集地理区域
+      if (bound) {
+        const p = new Promise((resolve, reject) => {
+          // 获取范围
+          const bdary = new BMap.Boundary();
+          // 获取行政区域, 行政区域的点有多少个
+          bdary.get(name, function (rs) {
+            const count = rs.boundaries.length;
+            if (count > 0) {
+              areaData[name] = rs.boundaries[0];
+            }
 
-            // 查找后结果
-            localSearch.setSearchCompleteCallback((searchResult) => {
-              // keyword
-              const keyword = searchResult.keyword;
-
-              // 坐标开启，使用LocalSearch自动搜集地理区域
-              if (curConf.coordinate) {
-                  // getPoi
-                  const poi = searchResult.getPoi(0);
-
-                  // 设置坐标
-                  itemData.longitude = poi.point.lng;
-                  itemData.latitude = poi.point.lat;
-              }
-
-              // 区域显示开启，使用LocalSearch自动搜集地理区域
-              if (bound) {
-                areaData[keyword] = searchResult.bounds;
-              }
-
-              resolve(keyword);
-            });
+            resolve(areaData[name]);
           });
+        });
 
-          // promises
-          promises.push(p);
-        }
+        // promises
+        promises.push(p);
+      }
+
+      // 坐标开启，使用LocalSearch自动搜集地理区域
+      if (curConf.coordinate) {
+        const p = new Promise((resolve, reject) => {
+
+          // 搜集地理位置信息
+          const localSearch = new BMap.LocalSearch(map);
+
+          // 根据具体名字查找
+          localSearch.search(name);
+
+          // 查找后结果
+          localSearch.setSearchCompleteCallback((searchResult) => {
+            // keyword
+            const keyword = searchResult.keyword;
+
+            // getPoi
+            const poi = searchResult.getPoi(0);
+
+            // 设置坐标
+            itemData.longitude = poi.point.lng;
+            itemData.latitude = poi.point.lat;
+
+            resolve(itemData);
+          });
+        });
+
+        // promises
+        promises.push(p);
+      }
     }
 
     return promises;
